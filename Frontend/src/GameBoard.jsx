7d049175a0a05b55288b6eef413fbce6d8da2999
Amr <lewolfje@gmail.com>
2023-05-11 19:12:19 +0300

    add piece placement, update the board and switch turns based on server response

--- Frontend/src/GameBoard.jsx
+++ Frontend/src/GameBoard.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/boardStyle.css";
 import Piece from "./Piece";
+import { useEffect } from "react";
+import { getTurnPlayer } from "./utils";
+
+async function getBoard() {
+  const response = await fetch(`http://localhost:8000/board/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data.board;
+}
+
+// <Piece key={x} x={x * 120 + 15} y={114} turn={} />
 
 function GameBoard() {
+  const [board, setBoard] = useState([]);
+  useEffect(() => {
+    getBoard().then((board) => {
+      setBoard(board);
+    });
+  }, []);
+
+  const Pieces = [];
+  for (let i = 0; i < board.length; i++) {
+    const row = board[i];
+    for (let j = 0; j < row.length; j++) {
+      const piece = row[j];
+      const turn = getTurnPlayer(piece);
+      if (piece !== 0) {
+        Pieces.push(
+          <Piece key={i * 7 + j} x={j * 120 + 15} y={i * 108} turn={turn} />
+        );
+      }
+    }
+  }
   return (
     <>
       <div className='board-container'>
         <div className='piece-container'>
           <div className='board'></div>
 
-          {[0, 1, 2, 3, 4, 5, 6].map((x) => {
-            return <Piece key={x} x={x * 120 + 15} y={114} />;
-          })}
+          {Pieces}
         </div>
       </div>
     </>
